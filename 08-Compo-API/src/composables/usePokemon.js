6875dc179e2334ae9bdc4580e3eb8bf662ec5c9f
YubalHormiga <yubalhh@gmail.com>
2024-01-10 10:43:10 +0100

    section 14 finished

--- 08-Compo-API/src/composables/usePokemon.js
+++ 08-Compo-API/src/composables/usePokemon.js
@@ -4,39 +4,40 @@ import axios from "axios";
 
 const usePokemon = () => {
 
-    const pokemon = ref()
-    const isLoading = ref(false)
-    const errorMessage = ref()
+    const pokemon = ref();
+    const isLoading = ref(false);
+    const errorMessage = ref();
     const router = useRouter();
-    const pokemonId = ref(1);
-
-
-
+    const defaultPokemonId = 1; // Valor por defecto
+    const pokemonId = ref(defaultPokemonId);
 
     const onSubmit = () => {
         console.log("Pokemon a buscar ", pokemonId.value);
         router.push({ name: "pokemon-id", params: { id: pokemonId.value } });
     };
 
-    const searchPokemon = async () => {
-        isLoading.value = true
-        pokemon.value = null
+    const searchPokemon = async (id) => {
+        isLoading.value = true;
+        pokemon.value = null;
+        
+        // Si no se proporciona un valor específico para id, utiliza el valor predeterminado
+        const searchId = id !== undefined ? id : defaultPokemonId;
 
+        // console.log('pokemonId en searchPokemon.vue:', searchId);
         try {
-            const url = `https://pokeapi.co/api/v2/pokemon/${pokemonId.value}`
-            console.log(url)
-            const { data } = await axios(url)
-            pokemon.value = data
-            errorMessage.value = null
-            console.log(data)
+            const url = `https://pokeapi.co/api/v2/pokemon/${searchId}`;
+            // console.log(url);
+            const { data } = await axios(url);
+            pokemon.value = data;
+            errorMessage.value = null;
 
         } catch (error) {
-            errorMessage.value = 'No se puedo cargar ese pokemon'
+            errorMessage.value = 'No se pudo cargar ese Pokémon';
         }
-        isLoading.value = false
+        isLoading.value = false;
     }
 
-    searchPokemon()
+    searchPokemon(); // Busca con el valor predeterminado
 
     return {
         pokemon,
@@ -46,9 +47,7 @@ const usePokemon = () => {
         router,
         pokemonId,
         onSubmit
-
     }
 }
 
-
-export default usePokemon
+export default usePokemon;
--- 08-Compo-API/src/views/Pokemon.vue
+++ 08-Compo-API/src/views/Pokemon.vue
@@ -1,26 +1,29 @@
 <script setup>
-import { useRoute } from "vue-router";
+import { useRoute, onBeforeRouteLeave } from "vue-router";
+import { watch, watchEffect } from "vue";
 import usePokemon from "../composables/usePokemon";
 
-const {
-  pokemon,
-  isLoading,
-  errorMessage,
-  searchPokemon,
-  router,
-  pokemonId,
-  onSubmit,
-} = usePokemon();
+const { pokemon, errorMessage, searchPokemon } = usePokemon();
+
 const route = useRoute();
-searchPokemon(route.params.id);
+
+watchEffect(() => {
+  searchPokemon(route.params.id);
+});
+
+onBeforeRouteLeave(() => {
+  const answer = window.confirm("¿Seguro de Salir?");
+  if (!answer) return false;
+});
 </script>
 
 <template>
   <h1 v-if="!pokemon && !errorMessage">Buscando</h1>
-  <h1 v-else-if="errorMessage" >{{ errorMessage }}</h1>
+  <h1 v-else-if="errorMessage">{{ errorMessage }}</h1>
   <div v-else>
     <h3>{{ pokemon.name }}</h3>
-
+    <img :src="pokemon.sprites.front_default" :alt="pokemon.name" />
+    <router-link :to="{ name: 'pokemon-search' }">Regresar</router-link>
   </div>
 </template>
 <style scoped></style>
